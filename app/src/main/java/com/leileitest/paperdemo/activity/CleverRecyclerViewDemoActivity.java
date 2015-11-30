8a575a35adac6264af76cb39b83c52e3894cb7e2
liulei9385 <457667329@qq.com>
2015-11-30 23:07:59 +0800

    增加图片缓存框架fresco

--- app/src/main/java/com/leileitest/paperdemo/activity/CleverRecyclerViewDemoActivity.java
+++ app/src/main/java/com/leileitest/paperdemo/activity/CleverRecyclerViewDemoActivity.java
@@ -1,6 +1,7 @@
 package com.leileitest.paperdemo.activity;
 
 import android.content.Context;
+import android.net.Uri;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -8,6 +9,7 @@ import android.view.ViewGroup;
 import android.widget.Adapter;
 import android.widget.TextView;
 
+import com.facebook.drawee.view.SimpleDraweeView;
 import com.leileitest.paperdemo.R;
 import com.leileitest.paperdemo.adapter.IBaseRecyclerAdapter;
 
@@ -25,11 +27,13 @@ public class CleverRecyclerViewDemoActivity extends BaseActivity {
 
     private CleverRecyclerView mCleverRecyclerView;
     private CleverAdapter cleverAdapter;
+    private List<String> pictureList;
 
     @Override
     public void initView() {
         mCleverRecyclerView = findView(R.id.mCleverRecyclerView);
-        mCleverRecyclerView.setOrientation(RecyclerView.VERTICAL);
+        //mCleverRecyclerView.setOrientation(RecyclerView.VERTICAL);
+        mCleverRecyclerView.setOrientation(RecyclerView.HORIZONTAL);
         cleverAdapter = new CleverAdapter(this, null);
         mCleverRecyclerView.setAdapter(cleverAdapter);
     }
@@ -46,15 +50,21 @@ public class CleverRecyclerViewDemoActivity extends BaseActivity {
          * 设置一页可以显示的item的数量
          * <p>注意：此方法必须在{@link CleverRecyclerView#setAdapter(Adapter)}之后调用
          */
-        mCleverRecyclerView.setVisibleChildCount(5);
+        mCleverRecyclerView.setVisibleChildCount(1);
     }
 
     @Override
     public void obtainData() {
         super.obtainData();
         List<String> stringList = new ArrayList<>();
-        for (int i = 0; i < 12; i++) {
-            stringList.add((i + 1) + "");
+        pictureList = new ArrayList<>();
+        pictureList.add("http://imgsrc.baidu.com/forum/pic/item/9f510fb30f2442a72c033b31d143ad4bd1130211.jpg");
+        pictureList.add("http://imgsrc.baidu.com/forum/pic/item/8dd7307adab44aed13fe4919b31c8701a38bfbee.jpg");
+        pictureList.add("http://pica.nipic.com/2008-01-05/200815121123445_2.jpg");
+        pictureList.add("http://e.hiphotos.baidu.com/zhidao/pic/item/38dbb6fd5266d01695ab94bf952bd40734fa35f2.jpg");
+        pictureList.add("http://pic2.nipic.com/20090506/1055421_080356081_2.jpg");
+        for (int i = 0; i < 5; i++) {
+            stringList.add("Item" + (i + 1));
         }
         cleverAdapter.addItemAll(stringList);
         cleverAdapter.notifyDataSetChanged();
@@ -74,13 +84,22 @@ public class CleverRecyclerViewDemoActivity extends BaseActivity {
                     parent, false));
         }
 
+        @Override
+        public void onBindViewHolder(CleverHolder holder, int position) {
+            super.onBindViewHolder(holder, position);
+            holder.itemText.setText(getItemAtPosition(position));
+            holder.draweeView.setImageURI(Uri.parse(pictureList.get(position)));
+        }
+
         public class CleverHolder extends RecyclerView.ViewHolder {
 
             TextView itemText;
+            SimpleDraweeView draweeView;
 
             public CleverHolder(View itemView) {
                 super(itemView);
                 itemText = findView(itemView, R.id.mText);
+                draweeView = findView(itemView, R.id.draweeView);
             }
         }
     }
--- app/src/main/java/com/leileitest/paperdemo/app/DemoApplication.java
+++ app/src/main/java/com/leileitest/paperdemo/app/DemoApplication.java
@@ -2,6 +2,13 @@ package com.leileitest.paperdemo.app;
 
 import android.app.Application;
 
+import com.facebook.drawee.backends.pipeline.Fresco;
+import com.facebook.imagepipeline.backends.okhttp.OkHttpImagePipelineConfigFactory;
+import com.facebook.imagepipeline.core.ImagePipelineConfig;
+import com.squareup.okhttp.OkHttpClient;
+
+import java.util.concurrent.TimeUnit;
+
 import io.paperdb.Paper;
 
 /**
@@ -15,6 +22,18 @@ public class DemoApplication extends Application {
     public void onCreate() {
         super.onCreate();
         Paper.init(getApplicationContext());
+        initFrescoImageLoader();
+    }
+
+    private void initFrescoImageLoader() {
+        OkHttpClient okHttpClient = new OkHttpClient();
+        okHttpClient.setConnectTimeout(10, TimeUnit.SECONDS);
+        ImagePipelineConfig config = OkHttpImagePipelineConfigFactory
+                .newBuilder(this, okHttpClient)
+                        // other setters
+                        // setNetworkFetchProducer is already called for you
+                .build();
+        Fresco.initialize(this, config);
     }
 
 }
