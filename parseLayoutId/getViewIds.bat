0c93cfc7950b9e81a1579bbfc8b8ea9b7ee35768
liulei <457667329@qq.com>
2018-12-21 13:11:59 +0800

    add parseLayoutId

---
+++ parseLayoutId/getViewIds.bat
@@ -0,0 +1,8 @@
+@if "%DEBUG%" == "" @echo off
+@rem ##########################################################################
+@rem
+@rem  Gradle startup script for Windows
+@rem
+@rem ##########################################################################
+@rem C:\develop\projects\java_pro\helloJava\doc\adapter_smartdev_list.xml
+java -classpath ".;dom4j-2.1.1.jar" com.leilei.demo.xml.TestXmlParser %1
